4eb6e187cbafb4f169835d31f11e45d97236f7df
bflournoy@gmail.com <elizabethflournoy@Elizabeths-MacBook-Pro.local>
2017-04-12 15:58:52 -0600

    regular checkers

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Your JS goes here
+console.log("poop");
+
+
+//getting body
+var body = document.getElementsByTagName("body")[0];
+
+// //appending tiles
+// body.appendChild(tile);
+// body.appendChild(tile2);
+
+
+
+////STANDARD CHECKERS
+for(var i = 0; i<81; i++){
+  var tile = document.createElement("div")
+  tile.style.float = "left"
+  tile.style.paddingBottom = "11.1%"
+  tile.style.width = "11.1%";
+  if(i%2===0){
+    tile.style.backgroundColor="gray";
+    body.appendChild(tile);
+  } else {
+    tile.style.backgroundColor="orange";
+    body.appendChild(tile);
+  }
+}
