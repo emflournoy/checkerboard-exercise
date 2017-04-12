b014e05cb725386b6070083b887f44c78838e3a4
Elizabeth Flournoy <bflournoy@gmail.com>
2017-04-12 16:33:17 -0600

    standard checkers

--- script.js
+++ script.js
@@ -26,26 +26,45 @@ for(var i = 0; i<81; i++){
 }
 
 
-////STANDARD CHECKERS
-for(var i = 0; i<81; i++){
-  var tile = document.createElement("div")
-  tile.style.float = "left"
-  tile.style.paddingBottom = "11.1%"
-  tile.style.width = "11.1%";
-  if(i%2===0){
-    tile.style.backgroundColor="gray";
-    body.appendChild(tile);
-  } else {
-    tile.style.backgroundColor="orange";
-    body.appendChild(tile);
-  }
-}
-//getting random color
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+// ////STANDARD CHECKERS
+// for(var i = 0; i<81; i++){
+//   var tile = document.createElement("div")
+//   tile.style.float = "left"
+//   tile.style.paddingBottom = "11.1%"
+//   tile.style.width = "11.1%";
+//   var color = getRandomColor()
+//   tile.style.backgroundColor=color;
+//   body.appendChild(tile);
+// }
+//
+// //getting random color
+// function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var fullColor = '#';
+//     for (var i = 0; i < 6; i++ ) {
+//         fullColor += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return fullColor;
+// }
+
+
+// ////CHANGING COLORS
+// for(var i = 0; i<81; i++){
+//   var tile = document.createElement("div")
+//   tile.style.float = "left"
+//   tile.style.paddingBottom = "11.1%"
+//   tile.style.width = "11.1%";
+//   var color = getRandomColor()
+//   var intervalID = scope.setInterval(getRandomColor, 2);
+//   tile.style.backgroundColor=color;
+//   body.appendChild(tile);
+// }
+// //getting random color
+// function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var fullColor = '#';
+//     for (var i = 0; i < 6; i++ ) {
+//         fullColor += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return fullColor;
+// }
