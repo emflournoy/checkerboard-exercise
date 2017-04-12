a40538744903abf7ae20180b7862023f0960f1ce
bflournoy@gmail.com <elizabethflournoy@Elizabeths-MacBook-Pro.local>
2017-04-12 16:03:37 -0600

    normal checkers

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ for(var i = 0; i<81; i++){
     body.appendChild(tile);
   }
 }
+
+
+////STANDARD CHECKERS
+for(var i = 0; i<81; i++){
+  var tile = document.createElement("div")
+  tile.style.float = "left"
+  tile.style.paddingBottom = "11.1%"
+  tile.style.width = "11.1%";
+  if(i%2===0){
+    tile.style.backgroundColor="gray";
+    body.appendChild(tile);
+  } else {
+    tile.style.backgroundColor="orange";
+    body.appendChild(tile);
+  }
+}
+//getting random color
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
